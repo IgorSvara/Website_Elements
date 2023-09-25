868dc8db5ce3be7d56dbf3a6cb57b3cd7d0c122d
igor <svaraigorw@gmail.com>
2023-09-25 09:21:25 +0200

    Changes

--- display_button_css/button.js
+++ display_button_css/button.js
@@ -1,12 +1,12 @@
 const button_c = document.querySelector('.common_button-container');
 const arrow = document.querySelector('.common_arrow');
-const follow_e = document.querySelector('.common_follow')
+// const follow_e = document.querySelector('.common_follow')
 
 button_c.addEventListener('click', () => {
     arrow.style.animation = `none`;
-    follow_e.style.animation = `none`;
+    // follow_e.style.animation = `none`;
     // Trigger a reflow (repaint) to apply the 'none' style immediately
     void arrow.offsetWidth;
-    arrow.style.animation = `moveArrow .4s ease-in forwards`;
-    follow_e.style.animation = `moveArrow .4s ease-in forwards`;
+    arrow.style.animation = `moveArrow .5s ease-in forwards`;
+    // follow_e.style.animation = `moveArrow .4s ease-in forwards`;
 });
